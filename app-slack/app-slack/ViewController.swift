38a6d15ad522ddc02693e834637aedb8f280360f
mailys <mailys.perez@epitech.eu>
2020-07-10 16:02:24 +0200

    initial xcode project

---
+++ app-slack/app-slack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-slack
+//
+//  Created by Maïlys Perez on 10/07/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

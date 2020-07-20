93116ed6869809e296fc62d8dc6bc098f177eb44
mailys <mailys.perez@epitech.eu>
2020-07-20 11:25:54 +0200

    [ADD] LoginVC UI

---
+++ app-slack/app-slack/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  app-slack
+//
+//  Created by Maïlys Perez on 20/07/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ app-slack/app-slack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  app-slack
+//
+//  Created by Maïlys Perez on 20/07/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"

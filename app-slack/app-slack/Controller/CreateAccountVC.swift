5b6a7148612d0603d207f9447f847ec59fe15985
mailys <mailys.perez@epitech.eu>
2020-07-22 11:56:29 +0200

    [ADD] register a user with Alamofire

--- app-slack/app-slack/Controller/CreateAccountVC.swift
+++ app-slack/app-slack/Controller/CreateAccountVC.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else
+            { return }
+        guard let pass = passwordTxt.text, passwordTxt.text != "" else
+            { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user !")
+            }
+        }
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
     }
 }
---
+++ app-slack/app-slack/Services/AuthService.swift
@@ -0,0 +1,67 @@
+//
+//  AuthService.swift
+//  app-slack
+//
+//  Created by Maïlys Perez on 21/07/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn : Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            return defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken : String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            return defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail : String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            return defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let header: HTTPHeaders = [
+            "Content-Type" : "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        AF.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            switch response.result {
+            case .success( _):
+                    completion(true)
+                case .failure(let error):
+                    completion(false)
+                    debugPrint(error)
+            }
+        }
+    }
+}

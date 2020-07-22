2b0b69ea97fdbdfc4ade7e097fc4138d1dd33989
mailys <mailys.perez@epitech.eu>
2020-07-22 17:21:22 +0200

    [ADD] possibility to create a user

--- app-slack/app-slack/Services/AuthService.swift
+++ app-slack/app-slack/Services/AuthService.swift
@@ -84,4 +84,39 @@ class AuthService {
             }
         }
     }
+    
+    func createUser(name: String, email: String, avatarName: String, avatarColor: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowerCaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor
+        ]
+        let header: HTTPHeaders = [
+            "Authorization":"Bearer \(AuthService.instance.authToken)",
+            "Content-Type" : "application/json; charset=utf-8"
+        ]
+        
+        AF.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON {
+            (response) in
+            
+            switch response.result {
+            case .success( _):
+                guard let data = response.data else { return }
+                let json = try! JSON(data: data)
+                let id = json["_id"].stringValue
+                let color = json["avatarColor"].stringValue
+                let avatarName = json["avatarName"].stringValue
+                let email = json["email"].stringValue
+                let name = json["name"].stringValue
+                
+                UserDataService.instance.setUserData(id: id, avatarColor: color, avatarName: avatarName, email: email, name: name)
+                completion(true)
+            case .failure(let error):
+                completion(false)
+                debugPrint(error)
+            }
+        }
+    }
 }
